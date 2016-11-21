8c8c87a7fd76d2a19933700b2a2023549ba42956
Wasin Thonkaew <haxpor@gmail.com>
2016-11-21 22:28:04 +0700

    Fix jsdoc comment for generating document

--- src/vendors/vendor.qrcode.generator.js
+++ src/vendors/vendor.qrcode.generator.js
@@ -22,7 +22,6 @@ module.exports = function(Playbasis) {
 	 * `type`: *Number* = 1-40  It affects the pattern of QR code. Default is 4.  
 	 * `error_correction_level`: *String* = 'L', 'M', 'Q', 'H'. Error correction level relates to how well QR code can restore data in case of damaged or dirty. Please refer [here](http://www.qrcode.com/en/about/error_correction.html) for more information. In most case, you don't need to modify this value. Default is 'L'.  
 	 * }
-	 * @return {String}          img tag string containing embedded Qr Code image.
 	 * @method  generate
 	 * @memberOf Playbasis.qrCode
 	 */
