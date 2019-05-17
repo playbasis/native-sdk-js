b9d2473ca9430cae0d7cb804b425263a622e3a05
Vorachet Jaroensawas <vorachet@gmail.com>
2019-05-17 12:10:19 +0700

    https://gitlab.com/playbasis/techops/issues/12 configuration file for Javascript SDK

---
+++ examples/js/config.js
@@ -0,0 +1,5 @@
+cfg_baseUrl = "https://api.pbapp.net";
+cfg_baseAsyncUrl = "https://api.pbapp.net/async/call";
+cfg_apiKey = "3017878153";
+cfg_apiSecret = "b507641c11088a33b731d4d79672fdbd";
+cfg_playerId = "game-demos-" + Math.floor(Math.random() * 1000000);
